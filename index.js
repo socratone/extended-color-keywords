27d30a7314cbc379b84df4144ba3a891ce35fa2b
socratone <gim2origin@gmail.com>
2021-05-22 02:31:21 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const colorTable = document.getElementById('color-table');
+colors.forEach((color) => {
+  colorTable.insertAdjacentHTML('beforeend', `<div class="color-cell">${color}</div>`)
+})
+
+const redColorTable = document.getElementById('red-color-table')
+redColors.forEach((color) => {
+  redColorTable.insertAdjacentHTML('beforeend', `<div class="color-cell">${color}</div>`)
+})
+
+const tables = document.querySelectorAll('.table');
+for (let i = 0; i < tables.length; i++) {
+  [...tables[i].children].forEach((cell) => {
+    const color = cell.textContent;
+    cell.textContent = '';
+    cell.style.backgroundColor = color;
+    cell.dataset.color = color;
+  
+    cell.addEventListener('mouseover', () => {
+      const tooltip = document.createElement('div');
+      tooltip.textContent = color;
+      tooltip.classList.add('tooltip');
+      cell.addEventListener(
+        'mouseleave',
+        () => {
+          tooltip.remove();
+        },
+        {
+          once: true,
+        }
+      );
+      cell.append(tooltip);
+    });
+  
+    cell.addEventListener('click', () => {
+      copyColorClipboard(color);
+      
+    });
+  });
+
+}
+
+function copyColorClipboard(color) {
+  const textarea = document.createElement('textarea');
+  textarea.textContent = color;
+  document.body.append(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+}
